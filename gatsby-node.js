20551a1dfc361eb8bd285507b78c1c10e75cd003
fai516 <yfsiuv@gmail.com>
2024-06-22 02:11:13 +0800

    update

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const fs = require('fs')
+
+const buildPath = path.join(__dirname, 'docs')
+
+exports.onPreInit = () => {
+    if (process.argv[2] === 'build') {
+        if (fs.existsSync(buildPath)) {
+            fs.rmdirSync(buildPath, { recursive: true })
+        }
+    }
+}
+
+exports.onPostBuild = () => {
+    fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'))
+}
