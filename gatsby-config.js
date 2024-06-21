9b0b647cd27ff0532777774c6cafb0578ad070a3
fai516 <yfsiuv@gmail.com>
2024-06-22 02:30:03 +0800

    update meta-tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,27 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `solanasummit-2024`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [ 'gatsby-plugin-postcss',"gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `solanasummit-2024`,
+        description: `Example project for the Gatsby Head API`,
+        twitterUsername: `@gatsbyjs`,
+        image: `/gatsby-icon.png`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
-};
+    plugins: [
+        'gatsby-plugin-postcss',
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+    ],
+}
