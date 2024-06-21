d8e596186135de15d8081d6ce9c883393cfb1dc5
fai516 <yfsiuv@gmail.com>
2024-06-22 03:49:47 +0800

    update

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -23,11 +23,10 @@ export const Seo = ({ title, description, pathname, children }) => {
 
     return (
         <>
-            <title>{seo.title}</title>
+            {/* <title>{seo.title}</title>
             <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} />
-            <meta name="twitter:image:src" content={seo.image} />
-            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="image" content={seo.image} /> */}
+            {/* <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={seo.title} />
             <meta name="twitter:url" content={seo.url} />
             <meta name="twitter:description" content={seo.description} />
@@ -37,15 +36,54 @@ export const Seo = ({ title, description, pathname, children }) => {
             <meta name="twitter:description" content={seo.description} />
             <meta name="twitter:creator" content={seo.twitterUsername} />
             <meta property="og:image" content={seo.image} />
-            <meta property="og:image:alt" content={seo.title} />
-            {/* <meta property="og:image:width" content="1240" />
-            <meta property="og:image:height" content="640" /> */}
-            <meta property="og:site_name" content={seo.title} />
+            <meta property="og:image:alt" content={seo.title} /> */}
+            {/* <meta property="og:site_name" content={seo.title} />
             <meta property="og:type" content="website" />
             <meta property="og:title" content={seo.title} />
             <meta property="og:url" content={seo.url} />
-            <meta property="og:description" content={seo.description} />
+            <meta property="og:description" content={seo.description} /> */}
             {/* <link rel="canonical" href={seo.url} /> */}
+
+            <title>VotingDAO - The largest decentralized voting</title>
+            <meta
+                name="title"
+                content="VotingDAO - The largest decentralized voting"
+            />
+            <meta
+                name="description"
+                content="VotingDAO is the first-ever global decentralized election on blockchain in which people can vote for the person they think has 
+        been the most influential in advancing blockchain development in the year. We strive to raise awareness of the importance of pop-voting systems in crypto space 
+        and explore practical use cases of incorporation of Web 3.0, DAO and NFTs."
+            />
+            <meta name="theme-color" content="#FFFFFF" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://www.votingdao.io/" />
+            <meta
+                property="og:title"
+                content="VotingDAO - The largest decentralized voting"
+            />
+            <meta
+                property="og:description"
+                content="Vote now for Blockchain Person of the Year 2021!"
+            />
+            <meta
+                property="og:image"
+                content="https://votingdao-nft-assets.s3.ap-southeast-1.amazonaws.com/Proof-of-Vote-static.jpg"
+            />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://www.votingdao.io/" />
+            <meta
+                property="twitter:title"
+                content="VotingDAO - The largest decentralized voting"
+            />
+            <meta
+                property="twitter:description"
+                content="Vote now for Blockchain Person of the Year 2021!"
+            />
+            <meta
+                property="twitter:image"
+                content="https://votingdao-nft-assets.s3.ap-southeast-1.amazonaws.com/Proof-of-Vote-static.jpg"
+            />
             {children}
         </>
     )
