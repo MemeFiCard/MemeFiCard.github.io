8018a1ba2bacf2dc264e88d8fa54f0fcef1c1ce6
fai516 <yfsiuv@gmail.com>
2024-06-22 13:42:44 +0800

    added correct preview pic size

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -42,47 +42,6 @@ export const Seo = ({ title, description, pathname, children }) => {
             <meta property="og:title" content={seo.title} />
             <meta property="og:url" content={seo.url} />
             <meta property="og:description" content={seo.description} />
-            {/* 
-            <title>VotingDAO - The largest decentralized voting</title>
-            <meta
-                name="title"
-                content="VotingDAO - The largest decentralized voting"
-            />
-            <meta
-                name="description"
-                content="VotingDAO is the first-ever global decentralized election on blockchain in which people can vote for the person they think has 
-        been the most influential in advancing blockchain development in the year. We strive to raise awareness of the importance of pop-voting systems in crypto space 
-        and explore practical use cases of incorporation of Web 3.0, DAO and NFTs."
-            />
-            <meta name="theme-color" content="#FFFFFF" />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://www.votingdao.io/" />
-            <meta
-                property="og:title"
-                content="VotingDAO - The largest decentralized voting"
-            />
-            <meta
-                property="og:description"
-                content="Vote now for Blockchain Person of the Year 2021!"
-            />
-            <meta
-                property="og:image"
-                content="https://votingdao-nft-assets.s3.ap-southeast-1.amazonaws.com/Proof-of-Vote-static.jpg"
-            />
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://www.votingdao.io/" />
-            <meta
-                property="twitter:title"
-                content="VotingDAO - The largest decentralized voting"
-            />
-            <meta
-                property="twitter:description"
-                content="Vote now for Blockchain Person of the Year 2021!"
-            />
-            <meta
-                property="twitter:image"
-                content="https://votingdao-nft-assets.s3.ap-southeast-1.amazonaws.com/Proof-of-Vote-static.jpg"
-            /> */}
             {children}
         </>
     )
